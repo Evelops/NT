39fecb5a09a476bc578b08f60d7377e82a86b3d2
eomseung-yeol <co6747@naver.com>
2023-12-16 23:01:14 +0900

    chore: setup nest dotenv config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  // adding for nest-config 
+  imports: [ConfigModule.forRoot({isGlobal: true}),UsersModule], 
+  controllers: [AppController, UsersController],
+  providers: [AppService,ConfigService, UsersService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  //.env에서 파일 가져올 때, process.env X => this.configService.get("data") => 형식으로 받아오도록 변경 
+  constructor(private readonly configService:ConfigService) {} 
+
+
   getHello(): string {
-    return 'Hello World!';
+    // return 'Hello World!';
+    return "hello"
   }
+
+  postHello(): string{
+    return 'hello post test';
+  }
+
+  
 }
+
+
